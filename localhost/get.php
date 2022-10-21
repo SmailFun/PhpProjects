5f9531b1ac22e4d903c0289efc380cc2bb574270
SmailFun <98034374+SmailFun@users.noreply.github.com>
2022-10-21 18:44:11 +0300

    Add files via upload

---
+++ localhost/get.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+
+
+echo "Введенная строка: " . "'" . $_GET['first_text']. "'" . " Длина: " . mb_strlen($_GET['first_text']) . " символов";
+
+?>
+
+
+</body>
+
+</html>
